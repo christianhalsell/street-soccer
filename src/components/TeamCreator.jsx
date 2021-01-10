a1b4476517024485777cfad69f18a6e10e9f0e16
Christian Halsell <shinobiwulf@gmail.com>
2021-01-10 13:37:04 -0800

    Add randomizer to team selection

--- src/components/TeamCreator.jsx
+++ src/components/TeamCreator.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
 
 const TeamCreator = () => {
   const teamRoster = []
-  // const teamRoster = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-  const teams = 4;
+  const numberOfTeams = 4;
   const numberOfPlayers = 17;
-  const round = [];
+  const teams = [];
 
   const addPlayers = () => {
     for (let i = 1; i <= numberOfPlayers; i++) {
@@ -15,17 +13,30 @@ const TeamCreator = () => {
   }
 
   const createRound = () => {
-    for (let i = 0; i < teams; i++) {
-      round.push([]);
+    for (let i = 0; i < numberOfTeams; i++) {
+      teams.push([]);
     }
   };
 
+  const randomizePlayers = (originalPlayers) => {
+    const arr = originalPlayers
+    for (var i = arr.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    
+    return arr;
+  }
+
   const generateTeams = () => {
+    const randomRoster = randomizePlayers(teamRoster);
     let i = 0;
-    for (let j = 0; j < teamRoster.length / teams; j++) {
-      for (let k = 0; k < teams; k++) {
+    for (let j = 0; j < teamRoster.length / numberOfTeams; j++) {
+      for (let k = 0; k < numberOfTeams; k++) {
         if (i < teamRoster.length) {
-          round[k].push(teamRoster[i])
+          teams[k].push(randomRoster[i])
           i++;
         }
       }    
@@ -38,18 +49,14 @@ const TeamCreator = () => {
 
   console.log(`Team Roster: ${teamRoster}`)
   console.log(`Number of Players: ${teamRoster.length}`);
-  console.log(`Number of Teams: ${teams}`);
-  console.log(round);
+  console.log(`Number of Teams: ${numberOfTeams}`);
+  console.log(teams);
 
   return (
     <div>
       //
     </div>
   )
-}
-
-TeamCreator.propTypes = {
-
 }
 
 export default TeamCreator
