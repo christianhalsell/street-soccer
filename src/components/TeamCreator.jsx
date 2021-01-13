3b340acd87e1d7f670d93de4189be8af44f81dfb
Christian Halsell <shinobiwulf@gmail.com>
2021-01-12 19:14:00 -0800

    Added commas to separate team players

--- src/components/TeamCreator.jsx
+++ src/components/TeamCreator.jsx
@@ -74,8 +74,8 @@ const TeamCreator = () => {
       <div>
         {teams.map((team, idx) => (
           <div key={team}><b>Team {idx + 1}:</b> {' '}
-          {team.map(player => (
-            <span key={player}>{player}{' '}</span>
+          {team.map((player, i) => (
+            <span key={player}>{player}{i < team.length - 1 ? ', ' : null}</span>
           ))}
           </div>
         ))}
